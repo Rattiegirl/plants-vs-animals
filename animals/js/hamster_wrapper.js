601314407850bfc7f11c4e4789f24200f8a832e2
Ratgirl <zlatogriviy.zherebyonok@gmail.com>
2021-07-08 09:40:11 -0700

    hamster_wrapper 1
    Co-authored-by: Naukomania <Naukomania@users.noreply.github.com>

---
+++ animals/js/hamster_wrapper.js
@@ -0,0 +1,68 @@
+function Hamster() {
+  const el =  document.querySelector ("#hamster_wrapper")
+  const humsterRunEl = el.querySelector("#hamster_run")
+  let x= 0;
+  let y= 0;
+  let step = 200;
+
+  this.direction="left"
+
+  const me = this;
+  this.render= function(){
+      el.style.left= x + "px"
+      el.style.top= y + "px"
+  }
+
+  this.sayHi = function(){
+      alert("Hi I want to chew you")
+  }
+
+  this.sit = function () {
+    el.setAttribute("data-mode", "sitting");
+  }
+
+  this.run = function ( ) {
+    el.setAttribute("data-mode","running" )
+  }
+
+  this.goUp = function (){
+      this.run();
+      this.direction="top";
+      humsterRunEl.style.transform="rotate(90deg)"
+      y=y-step
+  }
+
+  this.goDown = function (){
+    this.run();
+    this.direction="down";
+    humsterRunEl.style.transform="rotate(-90deg)"
+    y=y+step
+  }
+
+  this.goRight = function (){
+    this.run();
+    this.direction="right";
+    humsterRunEl.style.transform="scale(-1,1)"
+    x=x+step
+  }
+
+  this.goLeft = function (){
+    this.run();
+    this.direction="left";
+    humsterRunEl.style.transform="none"
+    x=x-step
+  }
+
+  setInterval(function(){
+    me.render();
+  },100)
+}
+
+const hamster = new Hamster();
+// hamster.sayHi();
+//hamster.sit();
+//hamster.run();
+//hamster.goUp();
+//hamster.goDown();
+// hamster.goRight();
+hamster.goLeft();
