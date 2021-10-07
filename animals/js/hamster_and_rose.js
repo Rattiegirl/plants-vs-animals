169192937f7abb1b45d1833c7be00e805da6b206
Ratgirl <zlatogriviy.zherebyonok@gmail.com>
2021-10-07 09:30:22 -0700

    GAME OVER! Winners are crowned

--- animals/js/hamster_and_rose.js
+++ animals/js/hamster_and_rose.js
@@ -10,6 +10,7 @@ rose.plant(
 
 const timer = document.querySelector(".timer")
 let seconds = 10
+const chewRose = 3
 const intervalId = setInterval(
     function () {
         if (paused) return
@@ -18,8 +19,20 @@ const intervalId = setInterval(
         if (seconds < 1) {
             seconds = 0
             clearInterval(intervalId)
-           pause()
-            alert("Game Over!")
+        
+            pause()
+            overlay.classList.remove("hidden")
+            overlay.style.display = "flex"
+            gameOverModal.classList.remove("hidden")
+            ////////////////////change winner-name, set crown
+            const winner = (animalPoints>=chewRose)?"hamster":"rose"
+            if (winner === "hamster") {
+                winnerAnimal.classList.add("with-crown");
+                winnerName.innerText = "славный герой Хомя!"
+            } else {
+                winnerPlant.classList.add("with-crown");
+                winnerName.innerText = "славная колючка Роза!"
+            }
         }
     }, 1000
 )
@@ -27,30 +40,42 @@ const intervalId = setInterval(
 const playPauseBtn = document.querySelector("#play-pause-btn")
 const overlay = document.querySelector(".overlay")
 const startGameBtn = document.querySelector(".start-game-btn")
-const play = function (){
+const startGameModal = document.querySelector(".start-game-modal")
+const gameOverModal = document.querySelector(".game-over-modal")
+const winnerName = document.querySelector(".winner-name")
+const winnerAnimal = document.querySelector(".winner-animal")
+const winnerPlant = document.querySelector(".winner-plant")
+const restartGameBtn = document.querySelector(".restart-game-btn")
+const play = function () {
     paused = false
     rose.plant(
         Math.floor(Math.random() * 1000),
         Math.floor(Math.random() * 600));
-    playPauseBtn.innerText="Pause"
+    playPauseBtn.innerText = "Pause"
 
 }
-const pause= function(){
+const pause = function () {
     paused = true
-    playPauseBtn.innerText="Play"
+    playPauseBtn.innerText = "Play"
 }
 playPauseBtn.onclick = function () {
-   //// alert("You started the game!")
+    //// alert("You started the game!")
     if (
         paused === true
     ) {
-       play()
+        play()
     } else {
-       pause()
+        pause()
     }
 
 }
 startGameBtn.onclick = function () {
-overlay.style.display="none"
-play()
+    overlay.classList.add("hidden")
+    setTimeout(function () {
+        play()
+        overlay.style.display = "none"
+        startGameModal.classList.add("hidden")
+    }, 300)
+
 }
+restartGameBtn.onclick = function () { location.reload() }
