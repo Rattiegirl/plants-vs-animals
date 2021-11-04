71ddf02895a1945774114b805637184f6562661f
Ratgirl <zlatogriviy.zherebyonok@gmail.com>
2021-11-04 08:34:58 -0700

    everything is broken down

--- app.js
+++ app.js
@@ -10,6 +10,21 @@ app.get("/", (req, res) => {
 app.get("/game", (req, res) => {
   res.send("The New Game");
 });
+app.get("/exaples/hamster-vs-rose", (req, res) => {
+  res.send("Friends not");
+});
+app.get("/welcome", (req, res) => {
+  res.send("You found this");
+});
+app.get("/team", (req, res) => {
+  res.send("Train some helpers");
+});
+app.get("/multiplayer", (req, res) => {
+  res.send("No more bots");
+});
+app.get("/scores", (req, res) => {
+  res.send("I show my trophies");
+});
 
 app.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}`);
