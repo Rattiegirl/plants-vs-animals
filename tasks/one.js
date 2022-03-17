7e0aefc8529d4f68b0a619afe606b3dfdfd0671f
RattieGirl <zlatogriviy.zherebyonok@gmail.com>
2022-03-17 09:30:40 -0700

    BAD STUFF ROBOTS INVADING

---
+++ tasks/one.js
@@ -0,0 +1,20 @@
+
+const getRandom = (n) => {
+    let number = Math.random() * n + 1
+    number = Math.floor(number)
+    number = (number + "").padStart(2, "0")
+    return number
+}
+for (let i = 0; i < 5; i += 1) {
+    console.log(getRandom(8))
+}
+const settings = {
+    body: 3,
+    face: 3,
+    eyes: 5,
+    mouth: 10,
+    accessory: 92
+}
+for ( let option in settings){
+    console.log(option, settings[option])
+}
