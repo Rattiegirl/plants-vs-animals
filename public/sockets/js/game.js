c24879b4e544c19af7da4206c45329d6cb931acd
RattieGirl <m_grigoryeva@outlook.com>
2023-05-30 09:09:32 -0700

    In sockets/game.html there are rectangles, circles and hamsters

--- public/sockets/js/game.js
+++ public/sockets/js/game.js
@@ -15,20 +15,35 @@ const sendAction = (data) => {
 
 const getAction = (data) => {
   const { type, name, command, params } = data;
+ 
   if (type === "animal") {
     const animal = game.animals[name];
     animal[command](params);
   }
-  console.log(1)
+  if (type === "plant") {
+    const plant = game.plants[name];
+    plant[command](params);
+  }
+ 
   if (type === "game"){
-    console.log(2)
+   
     if (name === "createAnimal"){
-      console.log(3)
+     
       const {type, id, l, w} = params
       if (type === "hamster"){
-        console.log(4)
+     
         const animal = getHamster({ name: `${type}-${id}`, l: l, w: w });
         game.addAnimal(animal)
+      }
+     
+    }
+    if (name === "createPlant"){
+     
+      const {type, id, l, w} = params
+      if (type === "rose"){
+     
+        const plant = getRose({ name: `${type}-${id}`, l: l, w: w });
+        game.addPlant(plant)
       }
      
     }
@@ -38,6 +53,7 @@ const getAction = (data) => {
 const getGame = () => {
   const el = document.querySelector("#game");
   const animals = {}
+  const plants = {}
 
   el.addEventListener('click', (e) => {
     const x = e.clientX;
@@ -55,6 +71,16 @@ const getGame = () => {
       });
       return;
     }
+    if (activePlant) {
+      // activeAnimal.goTo({x, y});
+      sendAction({
+        type: "plant",
+        name: activePlant.name,
+        command: "goTo",
+        params: { x, y }
+      });
+      return;
+    }
     console.log({ x, y });
   })
 
@@ -62,11 +88,21 @@ const getGame = () => {
 
   const setActiveAnimal = (animal) => {
     activeAnimal = animal;
+    activePlant = null;
     console.log("active animal is " + (animal.name || 'anonymous animal'))
     for (let animal of Object.values(animals)) {
       animal.el.classList.remove("active");
     }
     activeAnimal.el.classList.add("active");
+  }
+  const setActivePlant = (plant) => {
+    activePlant = plant;
+    activeAnimal = null;
+    console.log("active plant is " + (plant.name || 'anonymous plant'))
+    for (let plant of Object.values(plants)) {
+      plant.el.classList.remove("active");
+    }
+    activePlant.el.classList.add("active");
   }
 
   const addAnimal = (animal) => {
@@ -94,17 +130,55 @@ const getGame = () => {
     });
   }
 
+  const addPlant = (plant) => {
+    plants[plant.name] = plant;
+    el.append(plant.el);
+    plant.render();
+    plant.el.addEventListener('click', () => {
+      setActivePlant(plant);
+      const randomColor = "#" + Math.floor(Math.random() * (16 ** 3)).toString(16)
+      sendAction({
+        type: "plant",
+        name: plant.name,
+        command: "changeColor",
+        params: { color: randomColor }
+      });
+      const randomSize = Math.floor(Math.random() * 290) + 10
+      sendAction({
+        type: "plant",
+        name: plant.name,
+        command: "changeSize",
+        params: { size: randomSize }
+  
+  
+      })
+    });
+   
+  }
+  
+
   return {
     el,
     addAnimal,
+    addPlant,
     animals,
+    plants
   }
 }
+
+
+
+
 
 const game = getGame();
 
 const getHamster = (params = {}) => {
-  let el = null;
+  const firstHamster = document.querySelector("#first-hamster")
+  let el = firstHamster.cloneNode(true);
+  el.id = ""
+  el.classList.remove("hidden")
+  document.querySelector("#scene").appendChild(el)
+  const hamster = new Hamster(el, {width: 1200, height: 700})
   const name = params.name || "hamster";
 
   let w = params.w || 100;
@@ -118,6 +192,84 @@ const getHamster = (params = {}) => {
   let blockTime = 0;
   let isBlocked = false;
   let bgColor = "#e4b81b";
+
+  const changeColor = ({ color }) => {
+    // bgColor = color;
+    // el.style.backgroundColor = color
+  }
+
+  const changeSize = ({ size }) => {
+    // w = size;
+    // h = Math.floor(w * 0.7);
+    // el.style.width = `${w}px`;
+    // el.style.height = `${h}px`;
+  }
+
+  const goTo = ({ x, y }) => {
+    hamster.goTo(x, y)
+    // if (isBlocked) return;
+    // isBlocked = true;
+    // el.classList.add("is-blocked");
+
+    // destination = Math.sqrt((x - l) ** 2 + (y - t) ** 2);
+    // blockTime = Math.floor(destination * 1000 / speed);
+    // console.log({ destination, blockTime })
+    // l = x;
+    // t = y;
+    // render();
+
+    // setTimeout(() => {
+    //   isBlocked = false;
+    //   el.classList.remove("is-blocked");
+    // }, blockTime)
+  }
+
+  // const create = () => {
+  //   el = document.createElement("div");
+  //   el.classList.add("hamster");
+  //   el.classList.add("rose");
+  //   // el.innerHTML = ``
+  //   el.style.width = `${w}px`;
+  //   el.style.height = `${h}px`;
+  // }
+  // create();
+
+  const render = () => {
+    hamster.render()
+    // el.style.left = `${l}px`;
+    // el.style.top = `${t}px`;
+
+    // el.style.transition = `all ${blockTime}ms linear`
+  }
+
+  return {
+    el,
+    name,
+
+    goTo,
+    render,
+
+    changeColor,
+    changeSize,
+  }
+}
+
+const getRose = (params = {}) => {
+  let el = null;
+  const name = params.name || "rose";
+
+  let w = params.w || 100;
+  let h = Math.floor(w * 0.7);
+
+  let speed = params.speed || 1400; // 100px per second
+  let destination = 0;
+
+  let l = params.l || Math.floor(w / 2);
+  let t = params.t || Math.floor(h / 2);
+  let blockTime = 0;
+  let isBlocked = false;
+  let bgColor = "#e4b81b";
+ 
 
   const changeColor = ({ color }) => {
     bgColor = color;
@@ -152,6 +304,7 @@ const getHamster = (params = {}) => {
   const create = () => {
     el = document.createElement("div");
     el.classList.add("hamster");
+    el.classList.add("rose");
     // el.innerHTML = ``
     el.style.width = `${w}px`;
     el.style.height = `${h}px`;
@@ -163,6 +316,7 @@ const getHamster = (params = {}) => {
     el.style.top = `${t}px`;
 
     el.style.transition = `all ${blockTime}ms linear`
+    el.style.borderRadius = "50%"
   }
 
   return {
@@ -183,6 +337,12 @@ game.addAnimal(hamster1)
 const hamster2 = getHamster({ name: "hamster-2", l: 200, w: 70 });
 game.addAnimal(hamster2)
 
+const rose1 = getRose({ name: "rose-1", l: 200, w: 200 });
+game.addPlant(rose1)
+
+const rose2 = getRose({ name: "rose-2", l: 200, w: 200 });
+game.addPlant(rose2)
+
 const addAnimalButton = document.querySelector("#add-animal")
 
 addAnimalButton.addEventListener("click", () => {
@@ -196,6 +356,23 @@ addAnimalButton.addEventListener("click", () => {
       id: Date.now(),
       l: 100,
       w: 333
+    }
+  });
+})
+const addPlantButton = document.querySelector("#add-plant")
+
+addPlantButton.addEventListener("click", () => {
+ 
+  sendAction({
+    type: "game",
+    name: "createPlant",
+    // command: "goTo",
+    params: { 
+      type: "rose",
+      id: Date.now(),
+      l: 100,
+      w: 100,
+      
     }
   });
 })
